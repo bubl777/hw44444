9a712143ba0c8435074fc2db0635d0b94e596c42
buble <git@github.com:bubl777/helloworld.git>
2024-10-21 22:44:03 +0300

    commite houme

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 19 ;
+        if (age >= 18) {
+            System.out.println(" ты можешь вводить ");
+        }    else {
+            System.out.println(" Ты не можешь вводить");
+        }
+
+    }
+
+}
